462bd4df28c7c5668f79893fa11b4c58c1f33b04
rosyven <93422035+rosyven@users.noreply.github.com>
2023-09-25 17:07:00 +0300

    phonebook created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,74 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { Filter } from "./Filter/Filter";
+import { ContactList } from "./ContactList/ContactList";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleFilterChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { name, number, contacts } = this.state;
+
+    if (contacts.some(contact => contact.name === name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
+
+    const newContact = {
+      id: Date.now(),
+      name,
+      number,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+    }));
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+
+        <ContactForm
+          name={this.state.name}
+          number={this.state.number}
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit}
+        />
+
+        <h2>ontacts:</h2>
+        <Filter value={filter} onChange={this.handleFilterChange} />
+        <ContactList contacts={filteredContacts} onDelete={this.handleDelete} />
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+export class ContactForm extends Component {
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={this.props.name}
+            onChange={this.props.onChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label>
+          Number:
+          <input
+            type="tel"
+            name="number"
+            value={this.props.number}
+            onChange={this.props.onChange}
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+
+
+
